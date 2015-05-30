5fd84b5ea474d0459f677fda8ced93e784c23529
Ilya Bulah <ilya.bulah@gmail.com>
2015-05-30 10:14:45 +0300

    initial preview

---
+++ Extractor.php
@@ -0,0 +1,48 @@
+<?php
+
+class Extractor extends SimpleXMLElement {
+
+	public static function fromHtml($htmlString) {
+		$dom = new DOMDocument('1.0', 'UTF-8');
+		$dom->preserveWhiteSpace = false;
+
+		if (strlen($htmlString)){
+			libxml_use_internal_errors(true);
+			$dom->loadHTML($htmlString);
+			libxml_clear_errors();
+		}
+
+		return static::fromDom($dom);
+	}
+
+	public static function fromDom($dom) {
+		return simplexml_import_dom($dom, get_class());
+	}
+
+	public function cssPath($expression) {
+		return $this->xpath(XpathSubquery::get($expression));
+	}
+
+	public function xpathFirst($xpathQuery) {
+		$res = $this->xpath($xpathQuery);
+		if (count($res)) {
+			return $res[0];
+		}
+
+		return $res;
+	}
+
+	/**
+	 * @deprecated, for compatibility with nokogiri (@see)
+	 */
+	public function get($expression) {
+		return $this->cssPath($expression);
+	}
+
+	/**
+	 * @deprecated, for compatibility with nokogiri (@see)
+	 */
+	public function getElements($xpathQuery) {
+		return $this->xpath($xpathQuery);
+	}
+}
