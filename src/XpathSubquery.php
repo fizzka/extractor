ae7ab5f31bb9d7a5a008dc0c10169a62fceda9cb
Ilya Bulah <ilya.bulah@gmail.com>
2017-06-25 22:46:49 +0300

    invert condition

--- src/XpathSubquery.php
+++ src/XpathSubquery.php
@@ -30,59 +30,60 @@ class XpathSubquery
 
     private static function buildQuery($expression, $rel, $compile)
     {
-        $query = '';
+        if (!preg_match(self::REGEXP, $expression, $subs)) {
+            return;
+        }
 
-        if (preg_match(self::REGEXP, $expression, $subs)) {
-            $brackets = [];
-            if (isset($subs['id']) && '' !== $subs['id']) {
-                $brackets[] = "@id='" . $subs['id'] . "'";
+        $brackets = [];
+        if (isset($subs['id']) && '' !== $subs['id']) {
+            $brackets[] = "@id='" . $subs['id'] . "'";
+        }
+        if (isset($subs['attr']) && '' !== $subs['attr']) {
+            if (!(isset($subs['value']))) {
+                $brackets[] = "@" . $subs['attr'];
+            } else {
+                $attrValue = !empty($subs['value']) ? $subs['value'] : '';
+                $brackets[] = "@" . $subs['attr'] . "='" . $attrValue . "'";
             }
-            if (isset($subs['attr']) && '' !== $subs['attr']) {
-                if (!(isset($subs['value']))) {
-                    $brackets[] = "@" . $subs['attr'];
-                } else {
-                    $attrValue = !empty($subs['value']) ? $subs['value'] : '';
-                    $brackets[] = "@" . $subs['attr'] . "='" . $attrValue . "'";
-                }
-            }
-            if (isset($subs['class']) && '' !== $subs['class']) {
-                $brackets[] = 'contains(concat(" ", normalize-space(@class), " "), " ' . $subs['class'] . ' ")';
-            }
-            if (isset($subs['pseudo']) && '' !== $subs['pseudo']) {
-                if ('first-child' === $subs['pseudo']) {
-                    $brackets[] = '1';
-                } elseif ('last-child' === $subs['pseudo']) {
-                    $brackets[] = 'last()';
-                } elseif ('nth-child' === $subs['pseudo']) {
-                    if (isset($subs['expr']) && '' !== $subs['expr']) {
-                        $e = $subs['expr'];
-                        if ('odd' === $e) {
-                            $brackets[] = '(position() -1) mod 2 = 0 and position() >= 1';
-                        } elseif ('even' === $e) {
-                            $brackets[] = 'position() mod 2 = 0 and position() >= 0';
-                        } elseif (preg_match("/^[0-9]+$/", $e)) {
-                            $brackets[] = 'position() = '.$e;
-                        } elseif (preg_match("/^((?P<mul>[0-9]+)n\+)(?P<pos>[0-9]+)$/is", $e, $esubs)) {
-                            if (isset($esubs['mul'])) {
-                                $brackets[] = '(position() -' . $esubs['pos'] . ') mod '
-                                    . $esubs['mul'] . ' = 0 and position() >= ' . $esubs['pos'] . '';
-                            } else {
-                                $brackets[] = '' . $e . '';
-                            }
+        }
+        if (isset($subs['class']) && '' !== $subs['class']) {
+            $brackets[] = 'contains(concat(" ", normalize-space(@class), " "), " ' . $subs['class'] . ' ")';
+        }
+        if (isset($subs['pseudo']) && '' !== $subs['pseudo']) {
+            if ('first-child' === $subs['pseudo']) {
+                $brackets[] = '1';
+            } elseif ('last-child' === $subs['pseudo']) {
+                $brackets[] = 'last()';
+            } elseif ('nth-child' === $subs['pseudo']) {
+                if (isset($subs['expr']) && '' !== $subs['expr']) {
+                    $e = $subs['expr'];
+                    if ('odd' === $e) {
+                        $brackets[] = '(position() -1) mod 2 = 0 and position() >= 1';
+                    } elseif ('even' === $e) {
+                        $brackets[] = 'position() mod 2 = 0 and position() >= 0';
+                    } elseif (preg_match("/^[0-9]+$/", $e)) {
+                        $brackets[] = 'position() = '.$e;
+                    } elseif (preg_match("/^((?P<mul>[0-9]+)n\+)(?P<pos>[0-9]+)$/is", $e, $esubs)) {
+                        if (isset($esubs['mul'])) {
+                            $brackets[] = '(position() -' . $esubs['pos'] . ') mod '
+                                . $esubs['mul'] . ' = 0 and position() >= ' . $esubs['pos'] . '';
+                        } else {
+                            $brackets[] = '' . $e . '';
                         }
                     }
                 }
             }
-            $query = ($rel ? '/' : '//') .
-                ((isset($subs['tag']) && '' !== $subs['tag']) ? $subs['tag'] : '*') .
-                (($c = count($brackets)) ?
-                    ($c > 1 ? '[('.implode(') and (', $brackets) . ')]' : '[' . implode(' and ', $brackets) . ']')
-                : '')
-                ;
-            $left = trim(substr($expression, strlen($subs[0])));
-            if ('' !== $left) {
-                $query .= self::get($left, isset($subs['rel']) ? '>' === $subs['rel'] : false, $compile);
-            }
+        }
+
+        $query = ($rel ? '/' : '//') .
+            ((isset($subs['tag']) && '' !== $subs['tag']) ? $subs['tag'] : '*') .
+            (($c = count($brackets)) ?
+                ($c > 1 ? '[('.implode(') and (', $brackets) . ')]' : '[' . implode(' and ', $brackets) . ']')
+            : '')
+            ;
+        $left = trim(substr($expression, strlen($subs[0])));
+        if ('' !== $left) {
+            $query .= self::get($left, isset($subs['rel']) ? '>' === $subs['rel'] : false, $compile);
         }
 
         return $query;
